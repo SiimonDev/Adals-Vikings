4276f74b970f3d64e456f6b704e720530464eb70
Fredrik Boström <frebo233@gmail.com>
2015-01-26 09:31:48 +0100

    object

--- AdalsVikings/AdalsVikings/Object.h
+++ AdalsVikings/AdalsVikings/Object.h
@@ -1,8 +1,22 @@
 #pragma once
+#include <SFML\Graphics.hpp>
+#include <string>
+#include <vector>
+
 class Object
 {
 public:
-	Object();
+	Object(std::string objectID, std::string filePath);
 	~Object();
+
+	void render(sf::RenderWindow &window);
+	void update(sf::Time &time);
+
+private:
+	std::string filePath;
+	std::string mName, mLookAtDialog, mUsDialog, mCantUseDialog;
+	sf::Sprite mSprite;
+	bool mCanPickUp;
+
 };
 
