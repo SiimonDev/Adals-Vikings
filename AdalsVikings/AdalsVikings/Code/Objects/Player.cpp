ac6d72cd6bbd2109a965e438e2d9db58feb5274f
Tim0ntel <MGVguy@gmail.com>
2015-01-27 14:24:09 +0100

    Player fix
    Ska fixa mer sen

--- AdalsVikings/AdalsVikings/Code/Objects/Player.cpp
+++ AdalsVikings/AdalsVikings/Code/Objects/Player.cpp
@@ -5,18 +5,14 @@
 #include "..\Logics\PathFinder.h"
 
 
-sf::Vector2f Player::getPosition(){ return mPosition; }
-
-bool destinationReached = true;
-bool targetReached = true;
-sf::Vector2f velocity;
-
-int stepsTaken = 0;
-int totalSteps = 0;
-float alphaPerStep = 0;
-float currentAlpha = 0;
-
 Player::Player()
+	: mDestinationReached(true)
+	, mTargetReached(true)
+	, mVelocity(sf::Vector2f(0,0))
+	, mStepsTaken(0)
+	, mTotalSteps(0)
+	, mAlphaPerStep(0)
+	, mCurrentAlpha(0)
 {
 	rotationSpeed = 3;
 
@@ -29,11 +25,17 @@ Player::~Player(){
 
 }
 
+sf::Vector2f Player::getPosition()
+{ 
+	return mPosition; 
+}
+
 void Player::load(TileMap &tileMap, sf::Vector2f &spawnPosition){
 
 	mPosition.x = (PathFinder::getClosestFreeTile(tileMap, spawnPosition).x * tileMap.getTileSize().x) + tileMap.getTileSize().x / 2;
 	mPosition.y = (PathFinder::getClosestFreeTile(tileMap, spawnPosition).y * tileMap.getTileSize().y) + tileMap.getTileSize().y / 2;
 	ResourceManager::GetInstance().load(Textures::Player, "Assets/Images/Asteroid3.png");
+
 	ResourceManager::GetInstance().getTexture(Textures::Player).setSmooth(true);
 	mPlayerSprite.setTexture(ResourceManager::GetInstance().getTexture(Textures::Player));
 }
@@ -49,74 +51,74 @@ void Player::update(sf::Time &frameTime){
 void Player::walkPath(Path &path){
 	if (path.size() > 0)
 	{
-		currentPath = path;
+		mCurrentPath = path;
 
-		lastTarget = currentPath.at(currentPath.size() - 1);
-		currentPath.pop_back();
+		mLastTarget = mCurrentPath.at(mCurrentPath.size() - 1);
+		mCurrentPath.pop_back();
 
-		destinationReached = false;
-		targetReached = true;
+		mDestinationReached = false;
+		mTargetReached = true;
 	}
 }
 
 void Player::move(sf::Time &frameTime){
 
-	if (targetReached && !destinationReached)
+	if (mTargetReached && !mDestinationReached)
 	{
-		if (!currentPath.empty())
+		if (!mCurrentPath.empty())
 		{
-			currentTarget = currentPath.at(currentPath.size() - 1);
+			mCurrentTarget = mCurrentPath.at(mCurrentPath.size() - 1);
 
 			// Calculate what speed the player should be moving at.
 			// First we see what baspeed the player has (based on tha alpha of the current position).
 			// After this we calculate the difference in alpha between the current position and the target position.
 			// Last we determine the speed based on how long the player have to trawel (how big the alpha-difference is)
-			float bSpeed = ((mSpeed * currentTarget.color.a) / 255);
-			float alphaDiff = float(lastTarget.color.a) - float(currentTarget.color.a);
+			float bSpeed = ((mSpeed * mCurrentTarget.color.a) / 255);
+			float alphaDiff = float(mLastTarget.color.a) - float(mCurrentTarget.color.a);
 			float speed = bSpeed - (abs(alphaDiff) / 255);
 
 			// Calculate the angle toward the next target
-			// and then use this angle to create a velocity (the size of and angle of a "step")
-			distanceToTarget.x = currentTarget.position.x - mPosition.x;
-			distanceToTarget.y = currentTarget.position.y - mPosition.y;
-			float pointAngle = (float)atan2(distanceToTarget.y, distanceToTarget.x);
-			velocity = sf::Vector2f(cos(pointAngle), sin(pointAngle)) * speed;
+			// and then use this angle to create a mVelocity (the size of and angle of a "step")
+			mDistanceToTarget.x = mCurrentTarget.position.x - mPosition.x;
+			mDistanceToTarget.y = mCurrentTarget.position.y - mPosition.y;
+			float pointAngle = (float)atan2(mDistanceToTarget.y, mDistanceToTarget.x);
+			mVelocity = sf::Vector2f(cos(pointAngle), sin(pointAngle)) * speed;
 
 			// Calculate how many steps that has to be taken to reach the goal
-			// using pythagoras algorithm (the lenth of the distance vector / the length of the velocity vector)
-			float distanceLength = sqrt((distanceToTarget.x * distanceToTarget.x) + (distanceToTarget.y * distanceToTarget.y));
-			float velocityLength = sqrt(((velocity.x * velocity.x) + (velocity.y * velocity.y)));
-			totalSteps = distanceLength / velocityLength;
-			alphaPerStep = (alphaDiff / float(totalSteps));
-			currentAlpha = float(lastTarget.color.a);
+			// using pythagoras algorithm (the lenth of the distance vector / the length of the mVelocity vector)
+			float distanceLength = sqrt((mDistanceToTarget.x * mDistanceToTarget.x) + (mDistanceToTarget.y * mDistanceToTarget.y));
+			float mVelocityLength = sqrt(((mVelocity.x * mVelocity.x) + (mVelocity.y * mVelocity.y)));
+			mTotalSteps = distanceLength / mVelocityLength;
+			mAlphaPerStep = (alphaDiff / float(mTotalSteps));
+			mCurrentAlpha = float(mLastTarget.color.a);
 
-			lastTarget = currentTarget;
-			targetReached = false;
-			currentPath.pop_back();
+			mLastTarget = mCurrentTarget;
+			mTargetReached = false;
+			mCurrentPath.pop_back();
 		}
 		else
 		{
-			destinationReached = true;
+			mDestinationReached = true;
 		}
 	}
 
-	if (!targetReached && !destinationReached)
+	if (!mTargetReached && !mDestinationReached)
 	{
-		stepsTaken++;
+		mStepsTaken++;
 
-		// Take a step (position + the angle and lenths of a step (velocity))
-		mPosition += velocity;
+		// Take a step (position + the angle and lenths of a step (mVelocity))
+		mPosition += mVelocity;
 
 		// Incrementally reduce/increase the alpha every step.
-		currentAlpha -= alphaPerStep;
-		mScale.x = (1.f / 100.f) * currentAlpha;
-		mScale.y = (1.f / 100.f) * currentAlpha;
+		mCurrentAlpha -= mAlphaPerStep;
+		mScale.x = (1.f / 100.f) * mCurrentAlpha;
+		mScale.y = (1.f / 100.f) * mCurrentAlpha;
 
 		// Check if the total number if steps has been taken
 		// if it has then set the target as reached and reset the stepcounter
-		if (stepsTaken >= totalSteps){
-			targetReached = true;
-			stepsTaken = 0;
+		if (mStepsTaken >= mTotalSteps){
+			mTargetReached = true;
+			mStepsTaken = 0;
 		}
 	}
 }
